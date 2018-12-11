fb04cfba3bf9235c2f21d9bd3d0152f331443510
mrteddi <gmsmail@yahoo.com>
2018-12-11 15:08:43 -0800

    initial commit. wlgen v1

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# pctk
-Password Cracking ToolKit
+# Password Cracking ToolKit
+
+PCTK (Password Cracking ToolKit) is a small collection of scripts developed to aid in password cracking in capacities that standard utilities are unable to.
+
+## Contents:
+- [WLGen (Word List Generator)](README.md#WLGen])
+- More coming later.
+
+### WLGen
+WLGen (Word List Generator).
+Often passwords have a theme around their passwords and if you have a hint of what that may be, WLGen can help produce a wordlist to use as a dictionary.
+
+Requirements:
+```
+pip3 install -r requirements
+```
+Usage:
+```
+./wlgen.py --help
+usage: wlgen.py [-h] [-o O] [-n N] theme
+
+   __        ___     ____
+   \ \      / / |   / ___| ___ _ __
+    \ \ /\ / /| |  | |  _ / _ \ '_ \ 
+     \ V  V / | |__| |_| |  __/ | | |
+      \_/\_/  |_____\____|\___|_| |_|
+
+           by Conor Moening
+             Version: 1.0
+
+positional arguments:
+  theme       Word theme to generate a wordlist for
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -o O        File to output wordlist to. (Default: wordlist.txt)
+  -n N        Sets number of websites to scrape. (Default: 2)
+```
+Example:
+```
+We want to search for a wordlist of nouns, so the theme argument would be nouns.
+Search 4 sites and output to list.txt
+
+./wlgen.py nouns -n 4 -o list.txt
+   __        ___     ____
+   \ \      / / |   / ___| ___ _ __
+    \ \ /\ / /| |  | |  _ / _ \ '_ \ 
+     \ V  V / | |__| |_| |  __/ | | |
+      \_/\_/  |_____\____|\___|_| |_|
+
+[WLGen] Searching for 'List of nouns' in 4 websites
+[WLGen] Scraped 1743 words
+[WLGen] Wordlist saved to file 'list.txt'
+```
---
+++ wlgen.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+from googlesearch import search
+from urllib.request import urlopen
+import re
+import argparse
+
+version = "1.0"
+
+art = "   __        ___     ____\n"
+art += "   \ \      / / |   / ___| ___ _ __\n"
+art += "    \ \ /\ / /| |  | |  _ / _ \ '_ \ \n"
+art += "     \ V  V / | |__| |_| |  __/ | | |\n"
+art += "      \_/\_/  |_____\____|\___|_| |_|\n"
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+description=art + "\n\t   by Conor Moening" + "\n\t     Version: " + version)
+
+parser.add_argument('theme', help="Word theme to generate a wordlist for")
+parser.add_argument('-o', default="wordlist.txt", help="File to output wordlist to. (Default: wordlist.txt)")
+parser.add_argument('-n', type=int, default=2, help="Sets number of websites to scrape. (Default: 2)")
+args = parser.parse_args()
+
+urls = []
+words = set()
+
+print(art)
+
+f = open(args.o, "w+")
+
+print("[WLGen] Searching for 'List of ", args.theme, "' in ", args.n, " websites", sep='')
+
+for url in search('List of '+args.theme, num=args.n, stop=args.n):
+    page = urlopen(url)
+    baseUrl = re.search(r'\.(.*?)\.', url).group(1)
+    if baseUrl not in urls:
+        urls.append(baseUrl)
+
+        soup = BeautifulSoup(page, 'html.parser')
+        lis = soup.findAll(['li', 'td'])
+        for li in lis:
+            line = re.sub(' \((.*)\)|[^A-Za-z\\n]', '', li.text.strip()).lower()
+            if "http" not in line and "www" not in line and len(line) <= 32:
+                words.add(line)
+
+for i in sorted(words):
+        f.write(i + '\n')
+
+print("[WLGen] Scraped", len(words), "words")
+print("[WLGen] Wordlist saved to file '", args.o, "'", sep='')
+f.close()
