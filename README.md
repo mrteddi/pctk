7c3b6011e55ba8a4ec43a1fcea16250db1309071
mrteddi <gmsmail@yahoo.com>
2019-03-14 14:22:32 -0700

    leet.py 1.2

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ PCTK (Password Cracking ToolKit) is a small collection of scripts developed to a
 ## Contents:
 -  [WLGen (Word List Generator)](README.md#WLGen])
 -  [Leet (Leet Mutator)](README.md#Leet)
+-  [Change Log](README.md#Changelog)
 - More coming later.
 
-### WLGen
+## WLGen
 wlgen.py (Word List Generator).
+
 Often passwords have a theme around their passwords and if you have a hint of what that may be, WLGen can help produce a wordlist to use as a dictionary.
 
 Requirements:
@@ -20,14 +22,7 @@ Usage:
 ./wlgen.py --help
 usage: wlgen.py [-h] [-o O] [-n N] theme
 
-   __        ___     ____
-   \ \      / / |   / ___| ___ _ __
-    \ \ /\ / /| |  | |  _ / _ \ '_ \ 
-     \ V  V / | |__| |_| |  __/ | | |
-      \_/\_/  |_____\____|\___|_| |_|
-
-	   by Conor Moening
-	     Version: 1.0
+Written by Conor Moening - Version: 1.2
 
 positional arguments:
   theme       Word theme to generate a wordlist for
@@ -43,19 +38,14 @@ We want to search for a wordlist of nouns, so the theme argument would be nouns.
 Search 4 sites and output to list.txt
   
 ./wlgen.py nouns -n 4 -o list.txt
-   __        ___     ____
-   \ \      / / |   / ___| ___ _ __
-    \ \ /\ / /| |  | |  _ / _ \ '_ \ 
-     \ V  V / | |__| |_| |  __/ | | |
-      \_/\_/  |_____\____|\___|_| |_|
-  
 [WLGen] Searching for 'List of nouns' in 4 websites
 [WLGen] Scraped 1743 words
 [WLGen] Wordlist saved to file 'list.txt'
 ```
   
-## Leet
+## Leet Mutator
 leet.py (Leet Mutator)
+
 Hashcat and other password crackers can't produce every permutation of leet code for a given word,
 Leet Mutator is used to produce a leet version of a wordlist
  
@@ -66,10 +56,10 @@ None
 Usage:
 ```
 ./leet.py --help
-usage: leet.py [-h] [-o O] wordlist keyspace
+usage: leet.py [-h] [-o O] [-r R] wordlist keyspace
 
 Leet Mutator is a simple script to generate every leet variation of a wordlist.
-Written by Conor Moening - Version 1.0
+Written by Conor Moening - Version 1.2
 
 positional arguments:
   wordlist    Wordlist to create leet list from
@@ -79,14 +69,30 @@ positional arguments:
 optional arguments:
   -h, --help  show this help message and exit
   -o O        File to output mutations to (Default leet.txt)
+  -r R        GBs of RAM to use (Default 2)
 ```
 Example:
 ```
-./leet.py rockyou.txt e3
-[Leet] Running on rockyou.txt
+This runs the first 5 million lines in rockyou (with lines > 16 stripped)
+The keyspace e -> 3 o -> 0 i -> 1 is used
+./leet.py rockyouStripped.txt e3o0i1
+[Leet] Running on rockyouStripped.txt
 [Leet] This may take a while depending on the size of wordlist used
-[Leet] Mutations done. Saving to leet.txt
-[Leet] Failed mutations: 199
-[Leet] 23875008 mutations written
-[Leet] Finished. May take a moment to close
+[Leet] Flushing set and writing 21346057 mutations
+[Leet] Continuing... 
+[Leet] Mutations done. Saved to leet.txt
+[Leet] Mutate errors: 154
+[Leet] Finished. 21345903 mutations written
+```
+
+# Changelog
+#### Leet Mutator 1.2
+```
+- Changed looping so all cases are met
+- Fixed issue with RAM usage. ( See -r option )
+```
+#### Wordlist Generator 1.2
+```
+- Removed art
+- Added additional error check that handles python errors
 ```
--- leet.py
+++ leet.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-Version = "1.0"
+Version = "1.2"
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
 description="\nLeet Mutator is a simple script to generate every leet variation of a wordlist.\n" + 
@@ -11,29 +11,47 @@ parser.add_argument('wordlist', help="Wordlist to create leet list from")
 parser.add_argument('keyspace', help="Keyspace to use for substitutions\n" +
     "Ex) e3a5 will sub 3 for e and 5 for a")
 parser.add_argument('-o', default="leet.txt", help="File to output mutations to (Default leet.txt)")
+parser.add_argument('-r', type=int, default="2", help="GBs of RAM to use (Default 2)")
 args = parser.parse_args()
 
-def replaceN(word, char, n):
-    return word[:n] + str.encode(char) + word[n+1:]
+def printMutations( strings, fo, errors, total ):
+    print( "[Leet] Flushing set and writing " + str( len( strings ) ) + " mutations" )
+    for i in strings:
+        try:
+            fo.write( i.decode().strip() + "\n" )
+            total += 1
+        except:
+            errors += 1
+    strings.clear()
+    print( "[Leet] Continuing... " )
+    return errors, total
 
-def leet( word, keyspace, strings ):
+def replaceN(word, char, n):
+    return word[:n] + str.encode( char ) + word[n+1:]
+
+def leet( word, keyspace, strings, start ):
     for i in range( 0, len( keyspace ) ):
-        for j in range( 0, len(word) ):
-            if( chr(word[j]) == keyspace[i][0] ):
+        for j in range( start, len( word ) ):
+            if( chr( word[j] ) == keyspace[i][0] ):
                 first = replaceN( word, keyspace[i][1], j )
                 if first in strings:
                     return
                 strings.add( first )
-                leet( first, keyspace, strings )
+                leet( first, keyspace, strings, j )
 
 def main():
     keyspace = []
     strings = set()
 
+    total = 0
+    count = 0
+    errors = 0
+
     for i in range( 0, len( args.keyspace ), 2 ):
         keyspace.append( ( args.keyspace[i], args.keyspace[i + 1] ) )
 
     f = open( args.wordlist, "rb" )
+    fo = open( args.o, "w" )
 
     print( "[Leet] Running on " + args.wordlist )
     print( "[Leet] This may take a while depending on the size of wordlist used" )
@@ -41,19 +59,15 @@ def main():
     for word in f:
         if( len( word ) != 0 ):
             strings.add( word )
-            leet( word, keyspace, strings )
+            leet( word, keyspace, strings, 0 )
+            count += 1
+            if count % ( 3000000 * args.r ) == 0:
+                errors, total = printMutations( strings, fo, errors, total )
 
-    errors = 0
-    print( "[Leet] Mutations done. Saving to " + args.o )
-    f = open( args.o, "w")
-    for i in strings:
-        try:
-            f.write( i.decode().strip() + "\n")
-        except:
-            errors += 1
-    print( "[Leet] Failed mutations: " + str(errors) )
-    print( "[Leet] " + str(len(strings)) + " mutations written" )
-    print( "[Leet] Finished. May take a moment to close")
+    errors, total = printMutations( strings, fo, errors, total )
+    print( "[Leet] Mutations done. Saved to " + args.o )
+    print( "[Leet] Mutate errors: " + str( errors ) )
+    print( "[Leet] Finished. " + str( total ) + " mutations written" )
 
 if __name__ == '__main__':
     main()
